5d824dab2fba66aec9b2f7df9fa6ae1f2121e123
Morten Six <mort108v@stud.kea.dk>
2021-11-09 11:10:07 +0100

    Burger close  working on form, and header elements recalibrated

--- js/main.js
+++ js/main.js
@@ -31,6 +31,7 @@ listenForPlayerInput()
 
 
 function toggleMenu() {
+
     if (!showMenu) {
         hamburger.classList.add('open')
         nav.classList.add('open')
@@ -46,16 +47,27 @@ function toggleMenu() {
 
         showMenu = false
     }
+    if (showSignup) {
+
+        hamburger.classList.remove('open')
+        userForm.classList.remove('open')
+        formNav.classList.remove('open')
+        formItems.forEach(item => item.classList.remove('open'))
+
+        showSignup = false
+    }
 }
 
 function toggleForm() {
-    toggleMenu()
     if (!showSignup) {
         hamburger.classList.add('open')
         userForm.classList.add('open')
         formNav.classList.add('open')
         formItems.forEach(item => item.classList.add('open'))
         showSignup = true
+        nav.classList.remove('open')
+        menuNav.classList.remove('open')
+        navItems.forEach(item => item.classList.remove('open'))
     } else {
 
         hamburger.classList.remove('open')
